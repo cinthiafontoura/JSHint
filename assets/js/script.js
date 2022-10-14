3bf5bb1d4c6b96a6e62ff55e660a936004b16c58
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-10-14 11:41:48 +0100

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const API_KEY = "hXVcKIjNvN4sq4UjvF81FEDQJIk"
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"))
+
+document.getElementById("status").addEventListener("click", e => getStatus(e))
+
+async function getStatus(e) {
+    const querryString = `${API_URL}?api_key=${API_KEY}`
+
+    const response = await fetch(querryString)
+
+    const data = await response.json()
+
+    if(response.ok) {
+        console.log(data.expiry)
+    }
+}

76ea207d581119ccefbcae938b25440edb9fa2c2
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-10-15 13:29:30 +0100

    Create a exception modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,7 @@ async function postForm(e) {
   if(response.ok) {
     displayErrors(data)
   } else {
+    displayException(data)
     throw new Error(data.error)
   }
 }
@@ -71,6 +72,7 @@ function displayErrors(data) {
     if (response.ok) {
       displayStatus(data)
     } else {
+      displayException(data)
       throw new Error(data.error)
     }
   }
@@ -87,3 +89,17 @@ function displayErrors(data) {
     resultsModal.show()
   }
 }
+
+function displayException(data) {
+  let heading = `An Exception Occurred`
+
+  results = `<div> The API returned status code ${data.status_code}</div>`
+  results += `<div> Error number: <strong>${data.error_no}</strong></div>`
+  results += `<div> Error text: <strong>${data.error}</strong></div>`
+
+  document.getElementById("resultsModalTitle").innerText = heading
+  document.getElementById("results-content").innerHTML = results
+
+  resultsModal.show()
+}
+
